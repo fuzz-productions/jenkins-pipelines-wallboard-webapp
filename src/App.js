99e421716ff2c6f19de845b036bd8f27fcf190cc
Nick Trienens <nick@fuzzproductions.com>
2018-12-05 10:05:47 -0700

    [updated style and sorting]

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 import dotenv from 'dotenv';
 import axios from 'axios';
 import { List } from 'antd';
 import ProjectCell from './Components/ProjectCell'
 import 'antd/dist/antd.css';
+import './App.css';
 
 //require('dotenv').config()
 
@@ -16,50 +15,6 @@ class App extends Component {
 
     componentDidMount() {
         dotenv.config()
-
-        // let path = "https://jenkins.fuzzhq.com/job/ios-projects/job/m360-ios/api/json";
-        // //path = "data/m360.json";
-        // // , { "auth": {
-        // //     username: process.env.REACT_APP_USERNAME,
-        // //     password: process.env.REACT_APP_PASSWORD
-        // //   }}
-        // axios({
-        //     method: 'get', 
-        //     url: path,
-        //     auth: {
-        //       "username": process.env.REACT_APP_USERNAME,
-        //       "password": process.env.REACT_APP_PASSWORD
-        //     },
-        //   })
-        //   .then(res => {
-        //     let jobs = res.data.jobs;
-        //     jobs = jobs.sort(function(a, b) {
-        //         if (a.name === "dev") { return true }
-        //         if (b.name === "dev") { return false }
-        //         return a.name < b.name
-        //     });
-        //     console.log(jobs);
-
-        //     let job = {"projectName": res.data.name, "jobs": jobs, "id": res.data.fullName}
-        //     this.setState({ "projects": [job] });
-
-        //   })
-
-        //   axios.get("data/totr.json")
-        //     .then(res => {
-        //     let jobs = res.data.jobs;
-        //     jobs = jobs.sort(function(a, b) {
-        //        if (a.name === "dev") { return true }
-        //         if (b.name === "dev") { return false }
-        //         return a.name < b.name
-        //     });
-        //     console.log(jobs);
-
-        //     let job = {"projectName": res.data.name, "jobs": jobs}
-        //     var list = this.state.projects
-        //     this.setState({ "projects": list });
-
-        //   })
 
         axios.get("https://jenkins.fuzzhq.com/job/" + process.env.REACT_APP_GROUP + "/api/json", { "auth": {
              username: process.env.REACT_APP_USERNAME,
@@ -82,19 +37,23 @@ class App extends Component {
                   password: process.env.REACT_APP_PASSWORD
                 }})
                   .then(res => { 
-                    let jobs = res.data.jobs;
+                    var jobs = res.data.jobs;
                     jobs = jobs.filter( function (a) {
-                      if ( a.color === "notbuilt" || a.color === "notbuilt_anime" ) {
+                      if ( a.color === "notbuilt" || a.color === "notbuilt_anime" || a.color === "blue_anime" ) {
                           return false;
                       }
                       return true;
                     })
+                    console.log(jobs);
                     jobs = jobs.sort(function(a, b) {
-                       if (a.name === "dev") { return true }
-                        if (b.name === "dev") { return false }
-                        return a.name < b.name
+                        if (a.name === "dev" || a.name === "develop" || a.name === "master" ) { return -1 }
+                        if (b.name === "dev" || b.name === "develop" || b.name === "master") { return 1 }
+                        if ( a.name.toLowerCase() < b.name.toLowerCase() ){
+                          return -1
+                        }
+                        return 1
                     });
-                    //console.log(jobs);
+                    console.log(jobs);
                     job['jobs'] = jobs;
                     var list = this.state.projects
                     list.push(job)
@@ -117,11 +76,15 @@ class App extends Component {
    
     let {projects} = this.state
     projects = projects.sort(function(a, b) {
-                        return a.name < b.name
-                    });
+        console.log(a.jobs.length);
+          if( a['jobs'].length < b['jobs'].length ){
+            return -1
+          }
+          return 1
+    });
     return (
       <div className="App">
-        <header className="App-header">   Fuzz {process.env.REACT_APP_GROUPNAME}
+        <header className="App-header"> Fuzz {process.env.REACT_APP_GROUPNAME}
 
           <List 
             grid={{ gutter: 16, column: 3, offset: 5 }}
