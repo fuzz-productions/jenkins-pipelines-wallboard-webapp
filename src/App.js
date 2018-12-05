c4b069cddc00941875b6a9959e57a262b85e1630
Nick Trienens <nick@fuzzproductions.com>
2018-12-05 10:34:24 -0700

    [added requests for more data]

--- src/App.js
+++ src/App.js
@@ -16,6 +16,11 @@ class App extends Component {
     componentDidMount() {
         dotenv.config()
 
+        this.loadProjects();
+
+    }
+    
+    loadProjects = () => {
         axios.get("https://jenkins.fuzzhq.com/job/" + process.env.REACT_APP_GROUP + "/api/json", { "auth": {
              username: process.env.REACT_APP_USERNAME,
              password: process.env.REACT_APP_PASSWORD
@@ -25,6 +30,12 @@ class App extends Component {
             jobs = jobs.sort(function(a, b) {
                 return a.name < b.name
             });
+             jobs = jobs.filter( function (a) {
+                      if ( a.name === "koala-kit-ios" || a.name === "koala-pay-ios" || a.name === "tulerie-ios" || a.name === "coffers-ios" ) {
+                          return false;
+                      }
+                      return true;
+                    })
             //console.log(jobs);
 
             var arr = []
@@ -32,11 +43,12 @@ class App extends Component {
               var job = {"projectName": obj.name, "jobs": [], "id": obj.name}
 
               arr.push(
-                axios.get(obj.url + "api/json", { "auth": {
+                axios.get(obj.url + "api/json?tree=jobs[name,url,color,lastBuild[number,duration,building,result]]", { "auth": {
                   username: process.env.REACT_APP_USERNAME,
                   password: process.env.REACT_APP_PASSWORD
                 }})
                   .then(res => { 
+                    console.log(res);
                     var jobs = res.data.jobs;
                     jobs = jobs.filter( function (a) {
                       if ( a.color === "notbuilt" || a.color === "notbuilt_anime" || a.color === "blue_anime" ) {
