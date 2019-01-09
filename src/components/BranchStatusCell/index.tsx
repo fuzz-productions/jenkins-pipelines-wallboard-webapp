f8f4f13a863e511860c450ac39b33b61fa5f4b1f
fuzzagrosner <andrew@fuzzproductions.com>
2019-01-09 15:29:56 -0500

    [Builds] utilize grid for attentionzone. utilize list for stream. outline builds that failed.

--- src/components/BranchStatusCell/index.tsx
+++ src/components/BranchStatusCell/index.tsx
@@ -1,36 +1,41 @@
 import React from 'react'
 import { BuildInfoWithJob } from '../../model'
 import './styles.scss'
-import { Card, CardContent, Typography } from '@material-ui/core'
+import { Card, CardContent, GridListTile, Typography } from '@material-ui/core'
 import ReactImageFallback from 'react-image-fallback'
 import { getUIName } from '../../model/job_utils'
 import { userFriendlyFromLatestTime } from '../../model/build_utils'
+import fallbackIcon from '../../assets/ic_launcher.png'
 
 export interface BranchStatusCellProps {
   item: BuildInfoWithJob
+  isStream: boolean
 }
 
-export default function BranchStatusCell({ item }: BranchStatusCellProps) {
+export default function BranchStatusCell({ item, isStream }: BranchStatusCellProps) {
   let result = item.buildInfo.result
-  return <Card className="status-card">
-    <CardContent className="status-card-container">
-      <ReactImageFallback
-        className="status-job-image"
-        fallbackImage={'./../../assets/ic_launcher.png'}
-        src={`https://jenkins.fuzzhq.com/view/System/job/Job%20Icons/ws/${item.parentJobName}.png`} />
-      <div className="status-info-chunk">
-        <Typography component="h2"
-                    className="status-job-name"
-                    variant="h5">{getUIName(item.parentJobName)}</Typography>
-        <div className="status-container">
-          <div className={`status-circle ${result && result.toLowerCase() || 'null'}`} />
-          <Typography variant="h6"
-                      component="p">{item.job.name} - {item.buildInfo.displayName}</Typography>
+  const statusColorClass = `status-circle-${result && result.toLowerCase() || 'null'}`
+  return <GridListTile className={!isStream ? 'status-card-grid' : 'status-card-list'}>
+    <Card className="status-card">
+      <CardContent className={`status-card-container ${statusColorClass}-outline`}>
+        <ReactImageFallback
+          className="status-job-image"
+          fallbackImage={fallbackIcon}
+          src={`https://jenkins.fuzzhq.com/view/System/job/Job%20Icons/ws/${item.parentJobName}.png`} />
+        <div className="status-info-chunk">
+          <Typography component="h2"
+                      className="status-job-name"
+                      variant="h5">{getUIName(item.parentJobName)}</Typography>
+          <div className="status-container">
+            <div className={`status-circle ${statusColorClass}`} />
+            <Typography variant="h6"
+                        component="p">{item.job.name} - {item.buildInfo.displayName}</Typography>
+          </div>
+          <Typography
+            className="status-build-timestamp"
+            color="textSecondary">{userFriendlyFromLatestTime(item.buildInfo)}</Typography>
         </div>
-        <Typography
-          className="status-build-timestamp"
-          color="textSecondary">{userFriendlyFromLatestTime(item.buildInfo)}</Typography>
-      </div>
-    </CardContent>
-  </Card>
+      </CardContent>
+    </Card>
+  </GridListTile>
 }
--- src/pages/BuildList/index.tsx
+++ src/pages/BuildList/index.tsx
@@ -2,6 +2,7 @@ import React, { PureComponent } from 'react'
 import './style.scss'
 import { buildsHawk, BuildsProps } from '../../redux/builds/hawk'
 import BranchStatusCell from '../../components/BranchStatusCell'
+import { GridList } from '@material-ui/core'
 
 type Props = {
   isStream: boolean
@@ -12,9 +13,13 @@ class BuildList extends PureComponent<BuildsProps & Props> {
   render(): React.ReactNode {
     const list = this.props.isStream ? this.props.mainBuildList : this.props.unsuccessfulBuildsList
     return <>
-      {list.map((build) =>
-        <BranchStatusCell key={`${build.parentJobName}-${build.job.name}-${build.buildInfo.displayName}`}
-                          item={build} />)}
+      <GridList
+        cols={2}>
+        {list.map((build) =>
+          <BranchStatusCell key={`${build.parentJobName}-${build.job.name}-${build.buildInfo.displayName}`}
+                            isStream={this.props.isStream}
+                            item={build} />)}
+      </GridList>
     </>
   }
 }
