6671a77cd23b983bdd20f689824ac6a9b0e40372
fuzzagrosner <andrew@fuzzproductions.com>
2019-07-31 11:07:12 -0400

    [Builds] build list a functional component.

--- src/pages/BuildList.tsx
+++ src/pages/BuildList.tsx
@@ -1,4 +1,4 @@
-import React, { PureComponent } from 'react'
+import React from 'react'
 import { buildsHawk, BuildsProps } from '../redux/builds/hawk'
 import BuildItemTile from '../components/BuildItemTile'
 import { CircularProgress, GridList, Typography } from '@material-ui/core'
@@ -22,33 +22,29 @@ const StyledTypography = styled(Typography)`
   padding-right: 8px;
 `
 
-class BuildList extends PureComponent<BuildsProps & Props> {
-
-  render(): React.ReactNode {
-    const { isStream, unsuccessfulBuildsList, mainBuildList, buildsModel } = this.props
-    const list = isStream ? mainBuildList : unsuccessfulBuildsList
-    const isEmpty = list.length === 0
-    return <>
-      {buildsModel.isLoading && isEmpty && <StyledBuildListLoadingContainer>
-        <StyledTypography variant='h3'
-                          component='p'>Loading Builds</StyledTypography>
-        <CircularProgress size={50} />
-      </StyledBuildListLoadingContainer>}
-      {isEmpty && !buildsModel.isLoading && <div>
-        <Typography
-          variant='h1'
-          component='h1'
-        >All Builds Clear!</Typography>
-      </div>}
-      {!isEmpty && <GridList
-        cols={2}>
-        {list.map((build) =>
-          <BuildItemTile key={`${build.parentJobName}-${build.job.name}-${build.buildInfo.displayName}`}
-                         isStream={this.props.isStream}
-                         item={build} />)}
-      </GridList>}
-    </>
-  }
+const BuildList = ({ isStream, unsuccessfulBuildsList, mainBuildList, buildsModel }: BuildsProps & Props) => {
+  const list = isStream ? mainBuildList : unsuccessfulBuildsList
+  const isEmpty = list.length === 0
+  return <>
+    {buildsModel.isLoading && isEmpty && <StyledBuildListLoadingContainer>
+      <StyledTypography variant='h3'
+                        component='p'>Loading Builds</StyledTypography>
+      <CircularProgress size={50} />
+    </StyledBuildListLoadingContainer>}
+    {isEmpty && !buildsModel.isLoading && <div>
+      <Typography
+        variant='h1'
+        component='h1'
+      >All Builds Clear!</Typography>
+    </div>}
+    {!isEmpty && <GridList
+      cols={2}>
+      {list.map((build) =>
+        <BuildItemTile key={`${build.parentJobName}-${build.job.name}-${build.buildInfo.displayName}`}
+                       isStream={isStream}
+                       item={build} />)}
+    </GridList>}
+  </>
 }
 
 export default buildsHawk(BuildList)
